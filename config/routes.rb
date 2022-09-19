ca72ee19d7b8f713d77c9585a47af2fefcff3295
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-09-19 01:52:05 -0300

    Updated videoroom routes to fix errors pointed in the tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :videorooms, only: %w[new create]
+
   resources :videorooms, only: :show do
     resources :messages, only: :create
   end
-
-  resources :videorooms, only: %w[new create]
 end
