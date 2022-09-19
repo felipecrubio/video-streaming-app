a7d9a81acb36f07235980f3432924f3236b66828
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-09-19 17:29:26 -0300

    fixed redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   def store_user_location!
     # :user is the scope we are authenticating
     store_location_for(:user, request.fullpath) unless new_videoroom_path
-    store_location_for(:user, videoroom_path(Videoroom.last)) if new_videoroom_path
+    store_location_for(:user, videoroom_path(Videoroom.last.id + 1)) if new_videoroom_path
   end
 end
